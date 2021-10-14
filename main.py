7e0e411250958488d34601b64c2b66869f2666a6
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-10-14 09:53:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import Tkinter as Tk
+
+root = Tk.Tk()
+
+def submit():
+    print "entered text were " + entry.get()
+
+entry = Tk.Entry(root)
+entry.pack()
+button = Tk.Button(root,text='submit',command=submit)
+button.pack()
+
+root.mainloop()
 def georgeisdum():
   print("lmao")
 georgeisdum()
